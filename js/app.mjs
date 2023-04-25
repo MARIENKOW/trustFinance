65e54396f42161329515679dfb4c488e75bfe5cf
MARIENKOW <den.marienkow@gmail.com>
2023-04-25 05:56:12 +0300

    add sidebar and improved footer

--- js/app.mjs
+++ js/app.mjs
@@ -1,26 +1,29 @@
 import { smoothScroll } from "./module.js/smoothScroll.mjs";
 import { spoilerFlex } from "./module.js/spoiler.mjs";
 
-const sticky = [...document.querySelectorAll('[data-sticky]')];
-const main = document.querySelector('main');
+// const sticky = [...document.querySelectorAll('[data-sticky]')];
+// const main = document.querySelector('main');
 
 
 
 // window.addEventListener('scroll',fixedTop)
-// window.addEventListener('resize',fixedTop)
 
-function fixedTop(){
-   if(window.scrollY>=main.clientHeight - window.innerHeight){
-      const top = main.clientHeight - window.innerHeight;
-      sticky.forEach(el=>{
-            el.style.cssText=`
-               position:absolute;
-               top:${top}px;
-            `
-         })
-   }else{
-      sticky.forEach(el=>{
-         el.style.cssText=``;
-      })
-   } 
-}
+// function fixedTop(){
+//    if(window.scrollY>=main.clientHeight - window.innerHeight){
+//       const top = main.clientHeight - window.innerHeight;
+//       sticky.forEach(el=>{
+//             el.style.cssText=`
+//                position:absolute;
+//                top:${top}px;
+//             `
+//          })
+//    }else{
+//       sticky.forEach(el=>{
+//          el.style.cssText=``;
+//       })
+//    } 
+// }
+
+spoilerFlex()
+window.addEventListener('resize',spoilerFlex)
+
